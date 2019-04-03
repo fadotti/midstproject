batch gradient descent, TODO fix

src/perceptron.py
@@ -24,9 +24,9 @@ class perceptron:
         #training_type = "seq": controllo sequenziale del dataset
         #training_type = "rand": controllo casuale del dataset per un numero fissato di round
         if training_type=='seq':
-            rows = xrange(len(self.training_labels))
+            rows = range(len(self.training_labels))
         else:
-            rows = [np.random.randint(len(self.training_labels)) for i in xrange(self.max_rounds)]
+            rows = [np.random.randint(len(self.training_labels)) for i in range(self.max_rounds)]
         # uso tutte le righe del training set sequenzialmente
         dataset_rounds = 0 #numero di volte che ho ciclato sul dataset
         #numero di punti misclassificati

src/svm.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random as rn
-
+import math 
 class svm:
 
     def __init__(self, 
@@ -13,8 +13,9 @@ class svm:
                  correctly_classified = 0.95, 
                  max_rounds = 100000,
                  kernel = None,
+                 C = None,
                  cost_function = lambda w,X,y,C:(1/2) * np.sum(w**2) + C * np.sum(list(map(lambda xi,yi: max(0, 1 - yi * np.dot(xi,w)),X, y))),
-                 gradient_function = lambda w,x,yi,C:  w if (yi * np.dot(x,w)) >= 1 else w - C * (yi * x)
+                 hinge_function = lambda w,x,yi,C:  0 if (yi * np.dot(x,w)) >= 1 else  - C * (yi * x)
                  ):
         self.training_data = np.column_stack((np.matrix(training_data),np.negative(np.ones(training_data.shape[0]))))
         self.training_labels = np.array(training_labels)
@@ -28,20 +29,17 @@ class svm:
         self.max_rounds = max_rounds
         self.correctly_classified = correctly_classified
         self.kernel = kernel
-        self.C = 42
+        self.C = C if C is not None else self.__select_C()
         self.cost_function = cost_function
-        self.gradient_function = gradient_function
+        self.hinge_function = hinge_function
         
+
+    # STATICS 
+    def __select_C(self):
+        return 1
+
     
-    def predict(self,row):
-        return np.sign(np.dot(self.weights, np.squeeze(np.array(row)))).astype('int')
-        
-    def __update_weights(self,label,pattern):
-        self.weights =np.squeeze(np.array(self.weights - self.learn_rate*(self.weights - self.C * label * pattern)))
-        self.weights = self.weights/np.sqrt(sum(self.weights**2))
-
-    def stochastic_gradient_descent(self):
-
+    def __stochastic_gradient_descent(self):
         w = np.zeros(self.training_data.shape[1])
         min_w, min_f = None, float('inf') 
         l_rate = self.learn_rate
@@ -50,6 +48,7 @@ class svm:
 
         while no_improv < 100:
             f_value = self.cost_function(w,self.training_data, self.training_labels,self.C)
+            print(f_value)
             if f_value < min_f: 
                 min_w, min_f = w, f_value 
                 no_improv = 0
@@ -59,20 +58,50 @@ class svm:
             rn.shuffle(x)
 
             for i in x:
-                gradient_i = self.gradient_function(w,np.squeeze(np.array(self.training_data[i])), self.training_labels[i], self.C)
-                w = w - l_rate * gradient_i 
+                hinge_i = self.hinge_function(w,np.squeeze(np.array(self.training_data[i])), self.training_labels[i], self.C)
+                w = w - l_rate * (w+hinge_i)
             
         return min_w, min_f
-        
-    def train(self):
-        
+    
+    def __batch_gradient_descent(self,batch_size):
+        w = np.zeros(self.training_data.shape[1])
+        hinge_i= w
+        min_w, min_f = None, float('inf') 
+        l_rate = self.learn_rate 
+        x = list(range(self.training_data.shape[0]))
         for i in range(self.max_rounds):
-            n_row = np.random.randint(len(self.training_labels))
-            row = self.training_data[n_row]
-            label = self.training_labels[n_row]
-            y = self.predict(row)
-            if label*y < 1:
-                self.__update_weights(label,row)
+            f_value = self.cost_function(w,self.training_data, self.training_labels,self.C)
+            print(f_value)
+            #input("Press Enter to continue...")
+
+            if f_value < min_f: 
+                min_w, min_f = w, f_value 
+                
+            for k in range(math.ceil(len(x)/batch_size)):
+                for j in range(batch_size):
+                    index = j+k*(batch_size) 
+                    if(index < len(x)):
+                        hinge_i = hinge_i+self.hinge_function(w,np.squeeze(np.array(self.training_data[index])), self.training_labels[index], self.C)    
+                
+                w = w - l_rate * (w+hinge_i)
+            
+        return min_w, min_f 
+
+    # CLASS METHODS
+
+    def predict(self,row):
+        return np.sign(np.dot(self.weights, np.squeeze(np.array(row)))).astype('int')
+
+    
+    
+        
+    def train(self,method='stochastic',batch_size = None):
+        if batch_size==None:
+            batch_size=len(self.training_labels)
+        if method=='stochastic':
+            self.weights,min_f = self.__stochastic_gradient_descent()
+        elif method =='batch':
+            self.weights,min_f = self.__batch_gradient_descent(batch_size)
         return self.weights
     
     def test(self):
@@ -87,21 +116,7 @@ class svm:
         correct_classification = ((confusion_matrix[0,0]+confusion_matrix[1,1])/float(confusion_matrix.sum()))*100
         return confusion_matrix, correct_classification
 
-    def test_2(self,w):
-        w2 = self.weights
-        self.weights = w
-        confusion_matrix = np.matrix([[0, 0], [0, 0]])
-        current_row = 0
-        for row in self.test_data:
-            y = self.predict(row)
-            confusion_matrix[int((self.test_labels[current_row]+1)/2),int((y+1)/2)]=confusion_matrix[int((self.test_labels[current_row]+1)/2),int((y+1)/2)]+1
-
-            current_row += 1
-
-        correct_classification = ((confusion_matrix[0,0]+confusion_matrix[1,1])/float(confusion_matrix.sum()))*100
-        self.weights = w2
-        return confusion_matrix, correct_classification
-
+    
     
     
 

test-svm.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 scale = StandardScaler()
 #TRAINING SET SIMULATO
 #(X,y) = make_blobs(n_samples=1000, n_features=2, centers=2, cluster_std=1.05, random_state=40)#linearmente separabili 100% (cambio std)
-(X,y) = make_blobs(n_samples = 10000, n_features = 2, centers = 2, cluster_std = 1, random_state=12)
+(X,y) = make_blobs(n_samples = 10000, n_features = 2, centers = 2, cluster_std = 1, random_state=40)
 
 
 
@@ -38,17 +38,21 @@ Xt = np.asmatrix(Xt)
 yt[yt==0]=-1
  
 #percettrone
-p = svm(X, y, Xt, yt)
-p.train()
-a = p.weights
+p = svm(X, y, Xt, yt,C=10000,max_rounds=1000)
+""" p.train()
+w1 = p.weights
 print(p.test()[0])
 print(p.test()[1])
-print(p.weights)
+print(p.weights) """
+#w,tuttecose=p.minibatch_gradient_descent(30)
+
 w,tuttecose=p.stochastic_gradient_descent()
+print(w)
+print(tuttecose)
 xx = np.linspace(-2.5, 2.5)
 X1_std= scale.fit_transform(X1)
-a =  w[0]/w[1]
-yy = a*xx#-w[2]/w[1]
+a =  -w[0]/w[1]
+yy = a*xx
 plt.scatter(X1_std[:, 1], X1_std[:,2], marker='o', c=y)
 plt.plot(xx,yy)
 plt.plot(xx,yy+(1-w[2]/w[1]),linestyle='dashed',color='red')