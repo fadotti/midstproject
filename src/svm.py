4fe1b1444c983ee6a7eaad0d4216fede32da9133
RogueStatistician <simone.tosato95@gmail.com>
2019-04-05 10:29:22 +0200

    svm works

--- src/svm.py
+++ src/svm.py
@@ -15,13 +15,13 @@ class svm:
                  kernel = None,
                  C = None,
                  cost_function = lambda w,X,y,C:(1/2) * np.sum(w**2) + C * np.sum(list(map(lambda xi,yi: max(0, 1 - yi * np.dot(xi,w)),X, y))),
-                 hinge_function = lambda w,x,yi,C:  0 if (yi * np.dot(x,w)) >= 1 else  - C * (yi * x)
+                 hinge_function = lambda w,x,yi,C:  0 if (yi * np.dot(x,w)) >= 1  else  - C * (yi * x)
                  ):
-        self.training_data = np.column_stack((np.matrix(training_data),np.negative(np.ones(training_data.shape[0]))))
+        self.training_data = np.column_stack((np.matrix(training_data),np.ones(training_data.shape[0])))
         self.training_labels = np.array(training_labels)
         self.weights = np.zeros(self.training_data.shape[1])
         if test_data is not None:
-            self.test_data = np.column_stack((np.matrix(test_data), np.negative(np.ones(test_data.shape[0]))))
+            self.test_data = np.column_stack((np.matrix(test_data),np.ones(test_data.shape[0])))
         if test_labels is not None:
             self.test_labels = np.array(test_labels)
         self.learn_rate = learn_rate
@@ -32,15 +32,22 @@ class svm:
         self.C = C if C is not None else self.__select_C()
         self.cost_function = cost_function
         self.hinge_function = hinge_function
-        
-
+        self.treshold = 0.0001
     # STATICS 
     def __select_C(self):
         return 1
 
     
-    def __stochastic_gradient_descent(self):
-        w = np.zeros(self.training_data.shape[1])
+    
+    
+    # CLASS METHODS
+
+    def predict(self,row):
+        return np.sign(np.dot(self.weights, np.squeeze(np.array(row)))).astype('int')
+
+    
+    def train(self):
+        w = self.weights
         min_w, min_f = None, float('inf') 
         l_rate = self.learn_rate
         no_improv = 0 
@@ -49,7 +56,7 @@ class svm:
         while no_improv < 100:
             f_value = self.cost_function(w,self.training_data, self.training_labels,self.C)
             print(f_value)
-            if f_value < min_f: 
+            if abs(f_value - min_f)>self.treshold: 
                 min_w, min_f = w, f_value 
                 no_improv = 0
             else: 
@@ -59,50 +66,10 @@ class svm:
 
             for i in x:
                 hinge_i = self.hinge_function(w,np.squeeze(np.array(self.training_data[i])), self.training_labels[i], self.C)
-                w = w - l_rate * (w+hinge_i)
-            
-        return min_w, min_f
-    
-    def __batch_gradient_descent(self,batch_size):
-        w = np.zeros(self.training_data.shape[1])
-        hinge_i= w
-        min_w, min_f = None, float('inf') 
-        l_rate = self.learn_rate 
-        x = list(range(self.training_data.shape[0]))
-        for i in range(self.max_rounds):
-            f_value = self.cost_function(w,self.training_data, self.training_labels,self.C)
-            print(f_value)
-            #input("Press Enter to continue...")
-
-            if f_value < min_f: 
-                min_w, min_f = w, f_value 
-                
-            for k in range(math.ceil(len(x)/batch_size)):
-                for j in range(batch_size):
-                    index = j+k*(batch_size) 
-                    if(index < len(x)):
-                        hinge_i = hinge_i+self.hinge_function(w,np.squeeze(np.array(self.training_data[index])), self.training_labels[index], self.C)    
-                
-                w = w - l_rate * (w+hinge_i)
-            
-        return min_w, min_f 
-
-    # CLASS METHODS
-
-    def predict(self,row):
-        return np.sign(np.dot(self.weights, np.squeeze(np.array(row)))).astype('int')
-
-    
-    
-        
-    def train(self,method='stochastic',batch_size = None):
-        if batch_size==None:
-            batch_size=len(self.training_labels)
-        if method=='stochastic':
-            self.weights,min_f = self.__stochastic_gradient_descent()
-        elif method =='batch':
-            self.weights,min_f = self.__batch_gradient_descent(batch_size)
+                w = w - l_rate * (w/len(self.training_labels)+hinge_i)
+        self.weights = min_w    
         return self.weights
+
     
     def test(self):
         confusion_matrix = np.matrix([[0, 0], [0, 0]])
--- test-svm.py
+++ test-svm.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.svm import svm
 from sklearn.datasets.samples_generator import make_blobs
- 
+from sklearn import svm as _svm 
 from sklearn.preprocessing import StandardScaler 
 ###################################################################################
 scale = StandardScaler()
 #TRAINING SET SIMULATO
-#(X,y) = make_blobs(n_samples=1000, n_features=2, centers=2, cluster_std=1.05, random_state=40)#linearmente separabili 100% (cambio std)
-(X,y) = make_blobs(n_samples = 10000, n_features = 2, centers = 2, cluster_std = 1, random_state=40)
+#(X,y) = make_blobs(n_samples=1000, n_features=2, centers=2, cluster_std=1.05, random_state=62)#linearmente separabili 100% (cambio std)
+(X,y) = make_blobs(n_samples = 100000, n_features = 2, centers = 2, cluster_std = 1, random_state=99)
 
 
 
@@ -24,7 +24,7 @@ plt.show()
  
 #TEST SET SIMULATO
 #(X,y) = make_blobs(n_samples=3000, n_features=2, centers=2, cluster_std=1.05, random_state=40)#linearmente separabili 100%
-(Xt, yt) = make_blobs(n_samples=3000, n_features = 2, centers=2, cluster_std=1, random_state=12)
+(Xt, yt) = make_blobs(n_samples=3000, n_features = 2, centers=2, cluster_std=1, random_state=99)
  
 X1t=np.c_[np.ones((Xt.shape[0])),Xt]
 plt.scatter(X1t[:, 1],X1t[:, 2], marker='o', c=yt)
@@ -36,25 +36,40 @@ X = np.asmatrix(X)
 y[y==0]=-1
 Xt = np.asmatrix(Xt)
 yt[yt==0]=-1
- 
-#percettrone
-p = svm(X, y, Xt, yt,C=10000,max_rounds=1000)
-""" p.train()
-w1 = p.weights
-print(p.test()[0])
-print(p.test()[1])
-print(p.weights) """
-#w,tuttecose=p.minibatch_gradient_descent(30)
+X = scale.fit_transform(X)
+Xt = scale.fit_transform(Xt)
+p = svm(X, y, Xt, yt,C=1,max_rounds=100)
 
-w,tuttecose=p.stochastic_gradient_descent()
-print(w)
-print(tuttecose)
-xx = np.linspace(-2.5, 2.5)
+p.train()
+w=p.weights
+cose = _svm.SVC(kernel='linear')
+k = cose.fit(X,y)
+""" xx = np.linspace(-2.5, 2.5)
 X1_std= scale.fit_transform(X1)
 a =  -w[0]/w[1]
 yy = a*xx
 plt.scatter(X1_std[:, 1], X1_std[:,2], marker='o', c=y)
 plt.plot(xx,yy)
-plt.plot(xx,yy+(1-w[2]/w[1]),linestyle='dashed',color='red')
-plt.plot(xx,yy-(1-w[2]/w[1]),linestyle='dashed',color='red')
+plt.plot(xx,yy+(1-w[2]),linestyle='dashed',color='red')
+plt.plot(xx,yy-(1-w[2]),linestyle='dashed',color='red')
+plt.show() """
+
+
+
+
+Xt_std= scale.fit_transform(X1t)
+xx = np.linspace(-2.5, 2.5)
+b =  -k.coef_[0][0]/k.coef_[0][1]
+yy1 = b*xx
+w = w*np.sqrt(np.sum(w**2))
+a =  -w[0]/w[1]
+yy = a*xx
+plt.scatter(Xt_std[:, 1], Xt_std[:,2], marker='o', c=yt)
+plt.plot(xx,yy)
+plt.plot(xx,yy1)
+plt.plot(xx, yy1+(1-k.intercept_[0]/np.sqrt(np.sum(k.coef_[0]**2))),linestyle='dashed',color='orange')
+plt.plot(xx, yy1-(1-k.intercept_[0]/np.sqrt(np.sum(k.coef_[0]**2))),linestyle='dashed',color='orange')
+plt.plot(xx, yy+(1-w[2]),linestyle='dashed',color='red')
+plt.plot(xx, yy-(1-w[2]),linestyle='dashed',color='red')
+plt.scatter(k.support_vectors_[:,0],k.support_vectors_[:,1])
 plt.show()
